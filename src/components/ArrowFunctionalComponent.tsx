f9a7cd7bf4ee20bd492952a8be79ed51d9c79c3d
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-05-21 22:24:35 +0300

    feat: new components created

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,7 @@
+
+const ArrowFunctionalComponent = () => {
+    const title = "Is an Arrow functional component"
+    return <h1 className="text-center ">{title}</h1>
+}
+
+export default ArrowFunctionalComponent
---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,8 @@
+
+
+function FunctionalComponent() {
+    const title = "Is a functional component"
+    return <h1 className="text-center mt-12 text-xl font-bold ">{title}</h1>
+}
+
+export default FunctionalComponent;
