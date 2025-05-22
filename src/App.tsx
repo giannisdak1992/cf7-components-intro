25311f14ed51773f3317c3bfa0232b7342178060
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-05-23 00:23:22 +0300

    feat: using state in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWIthProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWIthPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWIthProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWIthPropsType.tsx";
+
+
+// import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+
+import Layout from "./components/Layout.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWIthState.tsx"; "./components/FunctionalComponent";
+// import ClassComponentWIthState from "./components/ClassComponentWIthState.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWIthProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWIthPropsType.tsx";
+
 
 
 function App() {
 
   return (
     <>
-        <div>
-            <ClassComponent  />
-            <FunctionalComponent />
-            <ArrowFunctionalComponent />
-            <ArrowFunctionalComponentWithProps title= "Is an arrow component with props" />
-            <ArrowFunctionalComponentWithPropsType title= "Is an arrow component with props" description="this is a description" />
-        </div>
+        <Layout>
+
+        {/*    <ClassComponent  />*/}
+        {/*    <FunctionalComponent />*/}
+        {/*    <ArrowFunctionalComponent />*/}
+        {/*    <ArrowFunctionalComponentWithProps title= "Is an arrow component with props" />*/}
+        {/*    <ArrowFunctionalComponentWithPropsType title= "Is an arrow component with props" description="this is a description" />*/}
+
+
+
+        {/*    <h1 className="text-center text-2xl font-bold">This is a heading1 title</h1>*/}
+        {/*        <FunctionalComponent />*/}
+        {/*    <ClassComponentWIthState />*/}
+
+            <FunctionalComponentWithState />
+        </Layout>
+
     </>
   )
 }
---
+++ src/components/FunctionalComponentWIthState.tsx
@@ -0,0 +1,29 @@
+import {useState} from "react";
+
+const FunctionalComponentWithState = () => {
+    const [count, setCount] = useState(0)
+
+    const increase = () => {
+        setCount(count+ 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+    return (
+        <>
+            <div className="space-y-4 pt-12">
+                <h1 className="text-center">Count is {count}</h1>
+                <div className="text-center space-x-4">
+                    <button className="bg-black text-white py-2 px-4" onClick={increase}>Increase</button>
+                    <button className="bg-red-400 text-white py-2 px-4" onClick={reset}>Reset</button>
+                </div>
+            </div>
+
+        </>
+
+    )
+
+}
+
+export default FunctionalComponentWithState
